cf6e1f30e03cca4adebc8679b3f78736acdaef75
Glow <770156137@qq.com>
2021-04-26 23:54:12 +0800

    2021.4.26家庭作业

---
+++ src/area/Test.java
@@ -0,0 +1,10 @@
+package area;
+
+public class Test {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(2.4f ,5.8f);
+        Round round = new Round(5.3f);
+        rectangle.calculatedArea();
+        round.calculatedArea();
+    }
+}
